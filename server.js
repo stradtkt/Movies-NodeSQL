47e2871e9af60dcb386984a909ddd061ca358dae
Kevin <stradtkt22@gmail.com>
2019-08-01 15:25:11 -0400

    Added different folders

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-const Sequalize = require('sequelize');
-
-const sequelize = new Sequalize({
-    dialect: 'sqlite',
-    storage: 'movies.db'
-});
-
-class Movie extends Sequalize.Model {
-
-}
-Movie.init({
-    title: Sequalize.STRING
-}, {sequelize});
+const db = require('./db');
+const { Movie } = db.models;
 
 (async () => {
-    await sequelize.sync({force: true});
-    try {
-        const movie = await Movie.create({
-            title: 'John Wick Chapter 2'
-        });
-        const movie2 = await Movie.create({
-            title: 'The Avengers, Endgame'
-        });
-        console.log(movie.toJSON());
-        console.log(movie2.toJSON());
-    } catch (error) {
-        console.error('Error connecting to the database', error);
-    }
+  await db.sequelize.sync({ force: true });
+  try {
+    const movie = await Movie.create({
+        title: 'John Wick Chapter 2'
+    });
+    console.log(movie.toJSON());
+
+    const movie2 = await Movie.create({
+        title: 'The Avengers, Endgame'
+    });
+    console.log(movie2.toJSON());
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
 })();
