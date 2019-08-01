cd79af20ec26c84ee043af0ee54735ceee1fd940
Kevin <stradtkt22@gmail.com>
2019-08-01 14:48:32 -0400

    Added create

--- server.js
+++ server.js
@@ -5,9 +5,24 @@ const sequelize = new Sequalize({
     storage: 'movies.db'
 });
 
+class Movie extends Sequalize.Model {
+
+}
+Movie.init({
+    title: Sequalize.STRING
+}, {sequelize});
+
 (async () => {
+    await sequelize.sync({force: true});
     try {
-        await sequelize.authenticate();
+        const movie = await Movie.create({
+            title: 'John Wick Chapter 2'
+        });
+        const movie2 = await Movie.create({
+            title: 'The Avengers, Endgame'
+        });
+        console.log(movie.toJSON());
+        console.log(movie2.toJSON());
     } catch (error) {
         console.error('Error connecting to the database', error);
     }
